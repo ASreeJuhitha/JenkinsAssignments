307d042ae2c0248fb0005b88c80eeb47183e9aff
ASreeJuhitha <sreejuhithaannadevara2017@gmail.com>
2023-10-18 14:20:47 +0530

    assignment3 java program

---
+++ Assignment3.java
@@ -0,0 +1,6 @@
+class Assignment3{  
+    public static void main(String args[]){  
+     System.out.println("first java program..."); 
+     System.out.println("Triggered jenkins build...");  
+    }  
+}
